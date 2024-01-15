2168c30f8c7c1a11cde43a33983ca012068a937f
anormalperson8 <133204516+anormalperson8@users.noreply.github.com>
2024-01-16 01:05:17 +0800

    misc: Updated comment to reflect parsing changes; Added an example

--- src/main/java/com/s151044/analytics/api/Course.java
+++ src/main/java/com/s151044/analytics/api/Course.java
@@ -1,4 +1,5 @@
 package com.s151044.analytics.api;
 
-// The course file is formatted as Dept,Code,Name,Grade,Credits,Is Major(boolean true/false),Semester
+// The course file is formatted as Dept;Code;Name;Grade;Credits;Is Major(boolean true/false);Semester
+// Example: COMP;1021;Introduction to Turtle;B;3;true;23-24 Fall
 public record Course(String dept, String code, String title, int credits, boolean isMajor, Grade grade, Semester semester) { }
